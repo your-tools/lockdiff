4f8a22245c3fde32ee8848bee41c5a1a5c8a3182
Dimitri Merejkowsky <dimitri@dmerej.info>
2023-12-21 18:07:22 +0100

    Move most of the code into src/lib, add tests

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,6 @@
-use std::fmt::Display;
-
-use anyhow::{bail, Context, Result};
-use serde::Deserialize;
-
-#[derive(Deserialize, Debug)]
-struct Lock {
-    package: Vec<Package>,
-}
-
-#[derive(Deserialize, Debug)]
-struct Package {
-    name: String,
-    version: String,
-}
-
-impl Display for Package {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}@{}", self.name, self.version)
-    }
-}
+use anyhow::Result;
+use lockdiff;
 
 fn main() -> Result<()> {
-    let args: Vec<_> = std::env::args().collect();
-    if args.len() != 2 {
-        bail!("Expected exactly one arg");
-    }
-
-    let lock_path = &args[1];
-    let lock_contents = std::fs::read_to_string(&lock_path).context("Could not read lock file")?;
-    let lock: Lock = toml::from_str(&lock_contents).context("Could not parse lock")?;
-    for package in lock.package {
-        println!("{package}");
-    }
-
-    Ok(())
+    lockdiff::run()
 }
