a019af281c66cd2b1e31c150a439d4a06d5ca8c3
Dimitri Merejkowsky <dimitri@dmerej.info>
2024-03-26 16:16:33 +0100

    Split lock formats per programming languages

---
+++ src/node.rs
@@ -0,0 +1,74 @@
+use std::collections::BTreeMap;
+
+use anyhow::{Context, Result};
+
+use crate::Package;
+
+use serde::Deserialize;
+#[derive(Deserialize, Debug, PartialEq, Eq)]
+struct NpmPackage {
+    version: String,
+}
+
+#[derive(Deserialize, Debug, PartialEq, Eq)]
+struct NpmLock {
+    packages: BTreeMap<String, NpmPackage>,
+}
+
+impl NpmLock {
+    pub(crate) fn packages(self) -> Vec<Package> {
+        self.packages
+            .into_iter()
+            .filter(|(k, _)| !k.is_empty())
+            .map(|(k, v)| Package::new(&k.replace("node_modules/", ""), &v.version))
+            .collect()
+    }
+}
+
+pub(crate) fn parse_npm(contents: &str) -> Result<Vec<Package>> {
+    let npm_lock: NpmLock =
+        serde_json::from_str(contents).context("Could not parse package-lock.json")?;
+    Ok(npm_lock.packages())
+}
+
+pub(crate) fn parse_yarn(contents: &str) -> Result<Vec<Package>> {
+    let entries = yarn_lock_parser::parse_str(contents).context("Could not parse yarn.lock")?;
+    Ok(entries
+        .iter()
+        .map(|e| Package::new(e.name, e.version))
+        .collect())
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_npm_lock() {
+        let contents = r#"
+{
+  "name": "my-proj",
+  "version": "0.0.0",
+  "lockfileVersion": 3,
+  "requires": true,
+  "packages": {
+    "": {
+      "name": "my-proj",
+      "version": "0.0.0",
+      "dependencies": {
+        "@eslint": "^2.1.2"
+      }
+    },
+    "node_modules/@eslint/eslintrc": {
+      "version": "2.1.2",
+       "resolved": "https://registry.npmjs.org/@eslint/eslintrc/-/eslintrc-2.1.2.tgz"
+    }
+  }
+}
+"#;
+
+        let packages = parse_npm(contents).unwrap();
+        assert_eq!(&packages, &[Package::new("@eslint/eslintrc", "2.1.2")]);
+    }
+}
---
+++ src/php.rs
@@ -0,0 +1,73 @@
+use anyhow::{Context, Result};
+use serde::Deserialize;
+
+use crate::Package;
+
+#[derive(Deserialize, Debug, PartialEq, Eq)]
+struct ComposerPackage {
+    name: String,
+    version: String,
+}
+
+#[derive(Deserialize, Debug, PartialEq, Eq)]
+#[serde(rename_all = "kebab-case")]
+struct ComposerLock {
+    packages: Vec<ComposerPackage>,
+    packages_dev: Vec<ComposerPackage>,
+}
+
+impl ComposerLock {
+    fn packages(self) -> Vec<Package> {
+        self.packages
+            .into_iter()
+            .chain(self.packages_dev.into_iter())
+            .map(|p| Package::new(&p.name, &p.version))
+            .collect()
+    }
+}
+
+pub(crate) fn parse_composer(contents: &str) -> Result<Vec<Package>> {
+    let composer_lock: ComposerLock =
+        serde_json::from_str(contents).context("Could not parse composer.json")?;
+    Ok(composer_lock.packages())
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_composer_lock() {
+        let contents = r#"
+{
+   "_readme" : [
+      "This file locks the dependencies of your project to a known state"
+   ],
+   "content-hash" : "657b85759b97fae05fc2983bd4abadf7",
+   "packages" : [
+      {
+         "name" : "acme.corp/stuff",
+         "source" : {},
+         "version" : "1.2.3"
+      }
+   ],
+   "packages-dev" : [
+      {
+         "name" : "myclabs/deep-copy",
+         "source" : {},
+         "version" : "1.11.0"
+      }
+   ]
+}
+"#;
+        let packages = parse_composer(contents).unwrap();
+        assert_eq!(
+            &packages,
+            &[
+                Package::new("acme.corp/stuff", "1.2.3"),
+                Package::new("myclabs/deep-copy", "1.11.0")
+            ]
+        );
+    }
+}
